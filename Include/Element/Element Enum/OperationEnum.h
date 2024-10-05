de60bc9a0f3d5c9ff332ccc856a3715471307df7
Kauê Anjos <kaueo.anjos@gmail.com>
2024-10-05 19:57:55 -0300

    feat(element): criando a classe element para operações

---
+++ Include/Element/Element Enum/OperationEnum.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum class Operacao {
+	elv,
+	rot,
+	mult,
+	div,
+	add,
+	sub
+};
---
+++ Include/Element/Element.h
@@ -0,0 +1,10 @@
+#include "Element Enum/OperationEnum.h"
+#pragma once
+class Element
+{
+public: 
+	// propriedades
+	double value;
+	Operacao operation;
+};
+
